a6020378c9ffca8be713c497bd31d1a1b1a9457d
Josh Trahan <josht0288@gmail.com>
2018-01-22 15:20:41 -0600

    rearrange contructor of ControllerServer

--- com/robut/rokrcsrv/ControllerServer.java
+++ com/robut/rokrcsrv/ControllerServer.java
@@ -20,37 +20,34 @@
 package com.robut.rokrcsrv;
 
 import java.io.IOException;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class ControllerServer {
-    private String bindAddr;
+    private InetAddress bindAddr;
     private int port;
 
     private String dbDir;
 
     private IRCManager ircManager;
 
-    public ControllerServer(String bindAddress, int port){
-        this.bindAddr = bindAddress;
+    public ControllerServer(int port, String dbDir, String bindAddress){
+        try {
+            this.bindAddr = InetAddress.getByName(bindAddress);
+        }
+        catch (UnknownHostException e){
+            System.err.printf("Error creating bind address: %s%n", e);
+        }
+
         this.port = port;
-
-        this.ircManager = new IRCManager();
-    }
-
-    public ControllerServer(String bindAddress, int port, IRCManager ircManager){
-        this.ircManager = ircManager;
-        this.bindAddr = bindAddress;
-        this.port = port;
-    }
-
-    public ControllerServer(int port, IRCManager ircManager){
-        this("0.0.0.0", port, ircManager);
+        this.ircManager = new IRCManager(dbDir);
     }
 
     public void startServer() throws IOException {
-        ServerSocket listener = new ServerSocket(this.port);
-        System.out.printf("Listening on port %d binded on %s%n", this.port, this.bindAddr);
+        ServerSocket listener = new ServerSocket(this.port, 50, this.bindAddr);
+        System.out.printf("Listening on port %d bound on %s%n", this.port, this.bindAddr);
 
         Socket controlSocket = listener.accept();
         System.out.printf("Connection made to client at address %s%n", controlSocket.getInetAddress());
